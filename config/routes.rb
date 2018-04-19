669efab525ecdfe39efbadf34427da326639d466
Kirsten Schumy <kirsten.schumy@gmail.com>
2018-04-19 14:10:22 -0700

    changed routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,87 +1,12 @@
 Rails.application.routes.draw do
-  get 'categories/index'
 
-  get 'categories/show'
-
-  get 'categories/new'
-
-  get 'categories/create'
-
-  get 'categories/edit'
-
-  get 'categories/update'
-
-  get 'categories/destroy'
-
-  get 'reviews/index'
-
-  get 'reviews/show'
-
-  get 'reviews/new'
-
-  get 'reviews/create'
-
-  get 'reviews/edit'
-
-  get 'reviews/update'
-
-  get 'reviews/destroy'
-
-  get 'order_items/index'
-
-  get 'order_items/show'
-
-  get 'order_items/new'
-
-  get 'order_items/create'
-
-  get 'order_items/edit'
-
-  get 'order_items/update'
-
-  get 'order_items/destroy'
-
-  get 'orders/index'
-
-  get 'orders/show'
-
-  get 'orders/new'
-
-  get 'orders/create'
-
-  get 'orders/edit'
-
-  get 'orders/update'
-
-  get 'orders/destroy'
-
-  get 'products/index'
-
-  get 'products/show'
-
-  get 'products/new'
-
-  get 'products/create'
-
-  get 'products/edit'
-
-  get 'products/update'
-
-  get 'products/destroy'
-
-  get 'merchants/index'
-
-  get 'merchants/show'
-
-  get 'merchants/new'
-
-  get 'merchants/create'
-
-  get 'merchants/edit'
-
-  get 'merchants/update'
-
-  get 'merchants/destroy'
+  resources :categories, only: [:create, :edit, :show, :index]
+  resources :reviews, only: [:create, :show]
+  resources :order_items, only: [:index]
+  resources :orders, only: [:new, :create, :edit, :show, :index]
+  resources :products, except: [:destroy]
+  patch '/products/:id/deactivate', to: "books#deactivate", as: 'deactivate_product'
+  resources :merchants, only: [:create, :show, :index]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
