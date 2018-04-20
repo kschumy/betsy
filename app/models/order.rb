a0c95f3c7d668bd052f2a3ed258feea8b9dd00ac
Jackie <jonofre16@hotmail.com>
2018-04-19 17:20:30 -0700

    Added validations to models

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,17 @@
 class Order < ApplicationRecord
   has_many :order_items
+
+  validates :email_address, presence: true
+  validates_format_of :email_address, :with => /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/
+
+  validates :mailing_address, presence: true
+  validates :cc_name, presence: true
+  validates :cc_number, presence: true
+  validates :cc_exp, presence: true
+  validates :cc_cvv, presence: true
+  validates :cc_zip, presence: true
+  validates :status, presence: true, inclusion: { in: %w(pending paid complete cancelled), message: "%{value} is not a valid status" }
+
+
+
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -3,4 +3,9 @@ class Product < ApplicationRecord
   has_many :reviews
   has_many :order_items
   belongs_to :merchant
+
+  validates :name, presence: true, uniqueness: true
+  validates :price, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: .01 }
+  validates :merchant, presence: true
+
 end
