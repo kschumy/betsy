77676fc7e293ce41c83d6e2a4add9822a8631bf8
Kirsten Schumy <kirsten.schumy@gmail.com>
2018-04-19 17:37:18 -0700

    refactored session controller for create and login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,30 +1,32 @@
 class SessionsController < ApplicationController
+
 	def create
 		auth_hash = request.env['omniauth.auth']
-
 		if auth_hash[:uid]
 			@merchant = Merchant.find_by(uid: auth_hash[:uid], provider: 'github')
-
 			if @merchant.nil?
 				@merchant = Merchant.build_from_github(auth_hash)
-				successful_save = @merchant.save
-				if successful_save
-					flash[:success] = "Logged in successfully"
-					session[:merchant_id] = @merchant.id
-					redirect_to root_path
+				if @merchant.save
+					successful_login
 				else
 					flash[:error] = "Some error happened in Merchant creation"
 					redirect_to root_path
 				end
 			else
-				flash[:success] = "Logged in successfully"
-				session[:merchant_id] = @merchant.id
-				redirect_to root_path
+				successful_login
 			end
-
 		else
 			flash[:error] = "Logging in through GitHub not successful"
 			redirect_to root_path
 		end
 	end
+
+	private
+
+	def successful_login
+		flash[:success] = "Logged in successfully"
+		session[:merchant_id] = @merchant.id
+		redirect_to root_path
+	end
+
 end
