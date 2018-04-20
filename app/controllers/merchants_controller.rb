08ba63eaf79526cc3462e43aea813b32971faa16
nicoletabrandolini <nicoletabrandolini@gmail.com>
2018-04-19 17:06:02 -0700

    Added methods for merchantscontroller

--- app/controllers/merchants_controller.rb
+++ app/controllers/merchants_controller.rb
@@ -1,14 +1,19 @@
 class MerchantsController < ApplicationController
+
   def index
+    @merchants = Merchant.all
   end
 
   def show
+    @merchant = Merchant.find_by(id: params[:id])
   end
 
   def new
   end
 
   def create
+    Merchant.create(merchant_params)
+    redirect_to merchants_path
   end
 
   def edit
@@ -19,4 +24,8 @@ class MerchantsController < ApplicationController
 
   def destroy
   end
+
+  private
+  def merchant_params
+    return params.require(:merchant).permit(:username, :email, :uid, :provider)
 end
