bd059723fcaa30c874cefab55d59f5c16ec89dd6
Kirsten Schumy <kirsten.schumy@gmail.com>
2018-04-20 12:58:33 -0700

    changed cc to be bigint

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180420000815) do
+ActiveRecord::Schema.define(version: 20180420195253) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -54,7 +54,7 @@ ActiveRecord::Schema.define(version: 20180420000815) do
   create_table "orders", force: :cascade do |t|
     t.string "email_address"
     t.string "cc_name"
-    t.integer "cc_number"
+    t.bigint "cc_number"
     t.integer "cc_cvv"
     t.integer "cc_zip"
     t.string "status"
