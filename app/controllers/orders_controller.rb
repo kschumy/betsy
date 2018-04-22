0c848533b3a5a9bb61cecc440c8a3d85134e949f
Cheeron Mars <cheeronmars+gh@posteo.org>
2018-04-22 09:53:40 -0700

    revised orders controller and relevant forms

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -9,23 +9,49 @@ class OrdersController < ApplicationController
   end
 
   def new
+    @order = Order.new
   end
 
   def create
-    Order.create(order_params)
-    redirect_to orders_path
+    @order  = Order.new(order_params)
+    #note: I'm not sure which is more suitable, the above or Order.create(order_params)
+    if @order.save
+      flash[:status] = :success
+      flash[:result_text] = "Successfully created order"
+      redirect_to order_items_path
+    else
+      flash[:status] = :failure
+      flash[:result_text] = "Order did not create"
+      flash[:messages] = @order.errors.messages
+      render :new
+      # raise
+    end
+
   end
 
   def edit
     @order = Order.find_by(id: params[:id])
     if @order.nil?
       redirect_to products_path
+    end
   end
 
   def update
     @order = Order.find_by(id: params[:id])
-    order.update(order_params)
-    redirect_to order_path(params[:id])
+    @order.update(order_params)
+    if @order.save
+      flash[:status] = :success
+      flash[:result_text] = "Successfully updated order"
+      # redirect_to order_items_path
+      redirect_to order_path(params[:id])
+    else
+      flash[:status] = :failure
+      flash[:result_text] = "Order did not update"
+      flash[:messages] = @order.errors.messages
+      render :edit
+
+      # redirect_to order_path(params[:id])
+    end
   end
 
   def destroy
@@ -33,5 +59,7 @@ class OrdersController < ApplicationController
 
   private
   def order_params
-    return params.require(:order). permit(:cc_name, :mailing_address, :email_address, :cc_number, :cc_exp, :cc_cvv, :cc_zip)
+    return params.require(:order). permit(:email_address, :cc_name, :email_address, :cc_number, :cc_exp_month, :cc_exp_year , :cc_cvv, :cc_zip, :status, :street , :customer_name ,:city, :state, :mailing_zip )
+  end
+
 end
