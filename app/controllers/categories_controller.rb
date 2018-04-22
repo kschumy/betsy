c32fdb3a6bee0cacbb5423967993549f57c1e187
Kirsten Schumy <kirsten.schumy@gmail.com>
2018-04-21 23:19:13 -0700

    changed zipcoded and ccv to be strings

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -54,8 +54,7 @@ class CategoriesController < ApplicationController
   private
 
   def category_params
-    return
-    params.require(:category).permit(:name)
+    return params.require(:category).permit(:name)
   end
 
 end
