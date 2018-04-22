885117a4418a68ed4765a32b798bd69ea3bce8f8
Kirsten Schumy <kirsten.schumy@gmail.com>
2018-04-22 16:27:01 -0700

    minor style changes to order model

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,29 +1,33 @@
 class Order < ApplicationRecord
   has_many :order_items
 
+  STATES = %w(
+    AK AL AR AZ CA CO CT DC DE FL GA HI IA ID IL IN KS KY LA MA MD ME MI MN MO
+    MS MT NC ND NE NH NJ NM NV NY OH OK OR PA RI SC SD TN TX UT VA VT WA WI WV WY
+  )
+
+  STATUS = %w(pending paid complete cancelled)
+
+
   validates :customer_name, presence: true
   validates :email_address, presence: true
   # validation of email is not restricted, only requires @
   validates_format_of :email_address, :with => /@/
   validates :cc_name, presence: true
-  validates :cc_number, numericality: true, presence: true, length: { is: 16 }
-  validates :cc_cvv, presence: true, numericality: true, length: { is: 3 }
-  validates :cc_zip, numericality: true, presence: true, length: { minimum: 5 }
+  validates :cc_number, presence: true, numericality: true, length: { is: 16 }
+  validates :cc_cvv, presence: true, length: { is: 3 }
+  validates :cc_zip, presence: true, length: { minimum: 5 }
   validates :street, presence: true
   validates :customer_name, presence: true
   validates :city, presence: true
-  validates :state, presence:true, inclusion: { in: :state_array, message: "%{value} is not a valid state"}
+  validates :state, presence:true, inclusion: { in: STATES, message: "%{value} is not a valid state"}
   validates :mailing_zip, numericality: true, presence: true, length: { minimum: 5 }
 
   validates :cc_exp_month, presence: true, numericality: { only_integer: true, in: 1..12 }
 
   validates :cc_exp_year, presence: true, numericality: { only_integer: true, minimum: Date.today.year }
 
-  validates :status, presence: true, inclusion: { in: %w(pending paid complete cancelled), message: "%{value} is not a valid status" }
-
-  def state_array
-    %w(AK AL AR AZ CA CO CT DC DE FL GA HI IA ID IL IN KS KY LA MA MD ME MI MN MO MS MT NC ND NE NH NJ NM NV NY OH OK OR PA RI SC SD TN TX UT VA VT WA WI WV WY)
-  end
+  validates :status, presence: true, inclusion: { in: STATUS, message: "%{value} is not a valid status" }
 
   def get_orders
     return find_orders
