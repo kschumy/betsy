9a9fb628264ddda8e045ef4542a45a6b76defc0c
nicoletabrandolini <nicoletabrandolini@gmail.com>
2018-04-24 21:24:49 -0700

    Testing controller/categorries

--- test/controllers/categories_controller_test.rb
+++ test/controllers/categories_controller_test.rb
@@ -1,39 +1,47 @@
 require "test_helper"
 
 describe CategoriesController do
+  let (:novelty) { categories(:novelty) }
+  let(:astro) { merchants(:astro) }
+
+
   it "should get index" do
-    get categories_index_url
+    get categories_path
     value(response).must_be :success?
   end
 
   it "should get show" do
-    get categories_show_url
+    get category_path(:id)
     value(response).must_be :success?
   end
 
-  it "should get new" do
-    get categories_new_url
-    value(response).must_be :success?
+  it "should not let a guest get new" do
+    get new_category_path
+    must_respond_with :not_found
   end
 
-  it "should get create" do
-    get categories_create_url
-    value(response).must_be :success?
+  it "should let a merchant get new" do
+    @merchant = merchants(:astro)
+    login(@merchant, :github)
+    get new_category_path
+    must_respond_with :success
   end
 
-  it "should get edit" do
-    get categories_edit_url
-    value(response).must_be :success?
+  it "should let a merchant create a new category" do
+    login(merchants, :github)
+    proc { post categories_path, params: {category: {name: "Marscrafts"}}}.must_change 'Category.count', 1
+    must_respond_with :redirect
   end
 
-  it "should get update" do
-    get categories_update_url
-    value(response).must_be :success?
+  it "should not let a guest create a new category" do
+    proc { post categories_path, params: {category: {name: "Marscrafts"}}}.must_change 'Category.count', 0
+    must_respond_with :not_found
   end
 
-  it "should get destroy" do
-    get categories_destroy_url
-    value(response).must_be :success?
+  it "must be a unique category" do
+    login(merchants(:astro), :github)
+    astro.name.must_equal "crafts"
+    proc { post categories_path, params: {category: {name: "crafts"}}}.must_change 'Category.count', 0
   end
 
 end
