d70301c50903919dc258bf2fe2cda759fe007f40
nicoletabrandolini <nicoletabrandolini@gmail.com>
2018-04-25 13:44:14 -0700

    Testing controller/review at 100%

--- test/controllers/reviews_controller_test.rb
+++ test/controllers/reviews_controller_test.rb
@@ -1,39 +1,45 @@
 require "test_helper"
 
 describe ReviewsController do
-  it "should get index" do
-    get reviews_index_url
-    value(response).must_be :success?
+  describe "Guest users" do
+
+    it "should get a new form" do
+      get new_product_review_path(products(:ball))
+      must_respond_with :success
+    end
+
+    it "renders 404 not_found" do
+      get new_product_review_path(-1)
+      must_respond_with :not_found
+    end
+
+    it "should be able to create a review" do
+      proc { post product_reviews_path(products(:ball)), params: { rating: 1}}.must_change 'Review.count', 1
+      must_respond_with :redirect
+    end
+
+    it "it won't create a review with bad data" do
+      proc { post product_reviews_path(products(:ball)), params: { rating: 7}}.must_change 'Review.count', 0
+      must_respond_with :bad_request
+    end
   end
 
-  it "should get show" do
-    get reviews_show_url
-    value(response).must_be :success?
-  end
+  describe "Logged in users" do
+    it "won't let a merchant review their own product" do
+      perform_login(merchants(:astro), :github)
+      get new_product_review_path(products(:ball))
+      must_respond_with :redirect
+      post product_reviews_path(products(:ball)), params: { rating: 5}
+      must_respond_with :redirect
+    end
 
-  it "should get new" do
-    get reviews_new_url
-    value(response).must_be :success?
-  end
+    it "will let other users vote for a product" do
+      perform_login(merchants(:astro), :github)
+      get new_product_review_path(products(:icecream))
+      must_respond_with :success
 
-  it "should get create" do
-    get reviews_create_url
-    value(response).must_be :success?
+      proc { post product_reviews_path(products(:icecream)), params: {rating: 5}}.must_change 'Review.count', 1
+      must_respond_with :redirect
+    end
   end
-
-  it "should get edit" do
-    get reviews_edit_url
-    value(response).must_be :success?
-  end
-
-  it "should get update" do
-    get reviews_update_url
-    value(response).must_be :success?
-  end
-
-  it "should get destroy" do
-    get reviews_destroy_url
-    value(response).must_be :success?
-  end
-
 end
