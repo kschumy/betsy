1f7b1b06c78f64a0b3c717eb6dfbb6bc0f74164a
Jackie <jonofre16@hotmail.com>
2018-04-25 21:23:08 -0700

    added mananage inventory route

--- config/routes.rb
+++ config/routes.rb
@@ -13,18 +13,19 @@ Rails.application.routes.draw do
   resources :order_items
   patch 'order_items/:id/mark_shipped', to: 'order_items#mark_shipped', as: 'mark_item_shipped'
   patch 'orders/:id/checkout_order', to: 'orders#checkout_order', as: 'checkout_order'
-    patch 'orders/:id/cancel_order', to: 'orders#cancel_order', as: 'mark_order_cancelled'
+  patch 'orders/:id/cancel_order', to: 'orders#cancel_order', as: 'mark_order_cancelled'
   resources :orders, only: [:new, :create, :edit, :update, :show, :index]
   resources :products, except: [:destroy] do
     resources :reviews, only: [:create, :new]
   end
   patch '/products/:id/deactivate', to: "products#deactivate", as: 'deactivate_product'
-get '/cart', to: "orders#cart", as: 'view_cart'
-# patch '/cart', to: "order#view_cart", as: 'view_cart'
+  get '/cart', to: "orders#cart", as: 'view_cart'
+  get '/manageinventory', to: "merchants#manage", as: 'manage_inventory'
+  # patch '/cart', to: "order#view_cart", as: 'view_cart'
   resources :merchants #, only: [:create, :show, :index]
   resources :merchants do
     resources :products, only: [:index]
   end
- get 'merchants/:id/orders', to: "merchants#order_fulfillment", as: 'merchant_orders'
+  get 'merchants/:id/orders', to: "merchants#order_fulfillment", as: 'merchant_orders'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
