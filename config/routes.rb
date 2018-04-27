9bc5f34a2709b28a89b4fd982b52262830954562
Cheeron Mars <cheeronmars+gh@posteo.org>
2018-04-26 20:08:56 -0700

    removed redundant merchant routes

--- config/routes.rb
+++ config/routes.rb
@@ -13,26 +13,27 @@ Rails.application.routes.draw do
   resources :order_items
   patch 'order_items/:id/mark_shipped', to: 'order_items#mark_shipped', as: 'mark_item_shipped'
   patch 'orders/:id/checkout_order', to: 'orders#checkout_order', as: 'checkout_order'
-    patch 'orders/:id/cancel_order', to: 'orders#cancel_order', as: 'mark_order_cancelled'
+  patch 'orders/:id/cancel_order', to: 'orders#cancel_order', as: 'mark_order_cancelled'
   resources :orders, only: [:new, :create, :edit, :update, :show, :index]
   resources :products, except: [:destroy] do
     resources :reviews, only: [:create, :new]
   end
   get '/manageinventory', to: "merchants#manage", as: 'manage_inventory'
   patch '/products/:id/deactivate', to: "products#deactivate", as: 'deactivate_product'
-get '/cart', to: "orders#cart", as: 'view_cart'
-# patch '/cart', to: "order#view_cart", as: 'view_cart'
-  resources :merchants #, only: [:create, :show, :index]
+  get '/cart', to: "orders#cart", as: 'view_cart'
+  # patch '/cart', to: "order#view_cart", as: 'view_cart'
+  #resources :merchants #, only: [:create, :show, :index]
   resources :merchants do
     resources :products, only: [:index]
+    resources :orders, only: [:show]
   end
-#Pretty sure we can delete the merchant order_items below
+  #Pretty sure we can delete the merchant order_items below
   # resources :merchants do
   #   resources :order_items, only: [:index]
   # end
-  resources :merchants do
-    resources :orders, only: [:show]
-  end
- get 'merchants/:id/orders', to: "merchants#order_fulfillment", as: 'merchant_orders'
+  # resources :merchants do
+  #
+  # end
+  get 'merchants/:id/orders', to: "merchants#order_fulfillment", as: 'merchant_orders'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
