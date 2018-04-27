55f21f225dac5efd149bc9253b489f51c3828956
Cheeron Mars <cheeronmars+gh@posteo.org>
2018-04-26 18:18:28 -0700

    update merchant_order show page

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -5,7 +5,11 @@ class OrdersController < ApplicationController
   end
 
   def show
-    @order = Order.find_by(id: params[:id])
+    if params[:merchant_id]
+      @order = Order.find_by(id: params[:id])
+    else
+      @order = Order.find_by(id: params[:id])
+    end
   end
 
   def new
