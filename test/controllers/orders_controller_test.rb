83125098c8c2545cd0a50f655cbbef678a73cdfd
nicoletabrandolini <nicoletabrandolini@gmail.com>
2018-04-27 07:50:22 -0700

    Testing orders_controllers

--- test/controllers/orders_controller_test.rb
+++ test/controllers/orders_controller_test.rb
@@ -1,39 +1,98 @@
 require "test_helper"
 
 describe OrdersController do
-  it "should get index" do
-    get orders_index_url
-    value(response).must_be :success?
+
+  describe "index" do
+    it "should get order index" do
+      get orders_path
+      value(response).must_be :success?
+    end
   end
 
-  it "should get show" do
-    get orders_show_url
-    value(response).must_be :success?
+  describe "show" do
+    it "should get show" do
+      perform_login(merchants(:astro), :github)
+      get order_path(orders(:star_mouse_order))
+      must_respond_with :success
+    end
   end
 
-  it "should get new" do
-    get orders_new_url
-    value(response).must_be :success?
+  describe "new" do
+    it "should be able to render a new form" do
+      get new_order_path
+      must_respond_with :success
+    end
   end
 
-  it "should get create" do
-    get orders_create_url
-    value(response).must_be :success?
+  describe "create" do
+    it "should create a new order" do
+      proc {
+        post orders_path, params: {
+          order: {
+            email_address: orders(:mystring_order).email_address,
+            cc_name: orders(:mystring_order).cc_name,
+            cc_number: orders(:mystring_order).cc_number,
+            cc_exp_month: orders(:mystring_order).cc_exp_month,
+            cc_exp_year: orders(:mystring_order).cc_exp_year,
+            cc_cvv: orders(:mystring_order).cc_cvv,
+            cc_zip: orders(:mystring_order).cc_zip,
+            status: orders(:mystring_order).status,
+            street: orders(:mystring_order).street,
+            customer_name: orders(:mystring_order).customer_name,
+            city: orders(:mystring_order).city,
+            state:orders(:mystring_order).state,
+            mailing_zip: orders(:mystring_order).mailing_zip,
+          }
+        }
+      }.must_change 'Order.count', 1
+      must_respond_with :redirect
+      must_redirect_to view_cart_path
+    end
+
+    it "should not create an invalid order" do
+      proc {
+        post orders_path, params: {
+          order: {
+            email_address: " ",
+            cc_name: orders(:mystring_order).cc_name,
+            cc_number: orders(:mystring_order).cc_number,
+            cc_exp_month: orders(:mystring_order).cc_exp_month,
+            cc_exp_year: orders(:mystring_order).cc_exp_year,
+            cc_cvv: orders(:mystring_order).cc_cvv,
+            cc_zip: orders(:mystring_order).cc_zip,
+            status: " ",
+            street: orders(:mystring_order).street,
+            customer_name: orders(:mystring_order).customer_name,
+            city: orders(:mystring_order).city,
+            state:orders(:mystring_order).state,
+            mailing_zip: orders(:mystring_order).mailing_zip,
+          }
+        }
+      }.must_change 'Order.count', 0
+      must_respond_with :bad_request
+    end
   end
 
-  it "should get edit" do
-    get orders_edit_url
-    value(response).must_be :success?
+  describe "edit" do
+    it "should provide a form to allow a user to edit an order" do
+      get edit_order_path(orders(:star_mouse_order).id)
+      must_respond_with :success
+    end
   end
 
-  it "should get update" do
-    get orders_update_url
-    value(response).must_be :success?
+  describe "checkout order" do
+
   end
 
-  it "should get destroy" do
-    get orders_destroy_url
-    value(response).must_be :success?
+  describe "cancel order" do
+    it "will update the status of a current order to cancelled" do
+      patch mark_order_cancelled_path(orders(:mystring_order).id)
+      must_respond_with :redirect
+      must_redirect_to root_path
+
+      updated_order = Order.find_by(id: orders(:mystring_order).id)
+      updated_order.status.must_equal "cancelled"
+    end
   end
 
 end
